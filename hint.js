51c600abdeaf084fb7dbf5b684bd0bbcf4ba188e
valentinavolgina2 <64236396+valentinavolgina2@users.noreply.github.com>
2020-12-18 18:26:01 -0800

    Add files via upload

--- hint.js
+++ hint.js
@@ -1,46 +1,50 @@
 
-var expectedAnswer = document.getElementById("txtExpected").textContent;
+const inputDiv = document.getElementById("txtAnswer");
+const hintField = document.getElementById("lblHint");
+const answerDiv = document.getElementById("txtExpected");
+const caseSens = document.getElementById("caseSens");
 
-function setExpectedAnswer(){
-	expectedAnswer = document.getElementById("txtExpected").textContent;
-}
+inputDiv.addEventListener('input',function(){
+    const currentAnswer = caseSens.checked ? inputDiv.textContent : inputDiv.textContent.toLowerCase();
+    const correctAnswer = caseSens.checked ? answerDiv.textContent : answerDiv.textContent.toLowerCase();
+	const editingString = evaluateAnswer(currentAnswer, correctAnswer);
 
-function hideHint() {
-	document.getElementById("lblHint").style.visibility = 'hidden';
-}
+	hintField.style.visibility = 'visible';
+	hintField.innerHTML = getHint(currentAnswer, editingString);
 
+});
 
-function checkAnswerOnInput()
-{
-	const currentAnswer = document.getElementById("txtAnswer").textContent;
-	const editing = evaluateAnswer(currentAnswer, expectedAnswer, false)
+inputDiv.addEventListener('focusout',()=>(hintField.style.visibility = 'hidden'));
+function getHint(answer, editingString) {
 
-	document.getElementById("lblHint").style.visibility = 'visible';
-	document.getElementById("lblHint").innerHTML = getHint(currentAnswer, editing);
-
-}
-
-function getHint(answer, editing) {
-
-	if (editing == "") {
-		return "<span style='color: green;font-weight: bold;'>" + answer + "<\/span>"
+    if (editingString === "") {
+		return "<span style='color: green;font-weight: bold;'>" + answer + "<\/span>";
 	}
 
-	var hint = "";
-	var answerIndex = 0;
-	for (var i = 0; i < editing.length; i++) {
-		if (editing.charAt(i) == "E") {
-			hint += "<span style='color: green;font-weight: bold;'>" + answer.charAt(answerIndex) + "<\/span>"
-			answerIndex++;
-		} else if (editing.charAt(i) == "C") {
-			hint += "<span style='color: red;font-weight: bold;'>" + answer.charAt(answerIndex) + "<\/span>"
-			answerIndex++;
-		} else if (editing.charAt(i) == "D") {
-			hint += "<span style='color: red;text-decoration:line-through;font-weight: bold;'>" + answer.charAt(answerIndex) + "<\/span>"
-			answerIndex++;
-		} else if (editing.charAt(i) == "I") {
-			hint += "<span style='color: red;font-weight: bold;'>_<\/span>"
-		}
+	let hint = "";
+    let idx = 0;
+	for (let i = 0; i < editingString.length; i++) {
+
+        const currentEditing = editingString.charAt(i);
+        const currentSymbol = answer.charAt(idx);
+        switch(currentEditing){
+            case "E":
+                hint += "<span class='correct'>" + currentSymbol + "<\/span>";
+                idx++;
+                break;
+            case "C":
+                hint += "<span class='wrong'>" + currentSymbol + "<\/span>";
+                idx++;
+                break;
+            case "D":
+                hint += "<span class='extra'>"+currentSymbol + "<\/span>";
+                idx++;
+                break;
+            case "I":
+                hint += "<span class='wrong'>_<\/span>";
+                break;
+        }
+
 	}
 	return hint;
 }
@@ -49,86 +53,63 @@ function getHint(answer, editing) {
 // ---------- distance editing algorithm  -----------------
 
 function diff(a, b) {
-	if (a == b) {
-		return 0;
-	}
-	else {
-		return 1;
-	}
+	return (a === b) ? 0 : 1; 
 }
 
-function evaluateAnswer(currentAnswer, expectedAnswer, caseSensitive) {
+function evaluateAnswer(currentAnswer, expectedAnswer) {
 
-	currentAnswer = currentAnswer.replace(String.fromCharCode(160), " "); //&nbsp;
-
-	if (!caseSensitive) {
-		currentAnswer = currentAnswer.toLowerCase();
-		expectedAnswer = expectedAnswer.toLowerCase();
-	}
-
-	if (expectedAnswer == currentAnswer) {
+	if (expectedAnswer === currentAnswer) {
 		return "";
 	}
 
-	var expectedLength = expectedAnswer.length;
-	var currentLength = currentAnswer.length;
+	const expectedLength = expectedAnswer.length;
+	const currentLength = currentAnswer.length;
 
-	var d = Array.from(Array(currentLength + 1), () => new Array(expectedLength + 1));
+	let d = Array.from(Array(currentLength + 1), () => new Array(expectedLength + 1));
+	let moves = Array.from(Array(currentLength + 1), () => new Array(expectedLength + 1));
 
-	for (var i = 0; i < currentLength + 1; i++) {
-		d[i][0] = i;
-	}
-	for (var j = 0; j < expectedLength + 1; j++) {
-		d[0][j] = j;
-	}
-
-	var moves = Array.from(Array(currentLength + 1), () => new Array(expectedLength + 1));
-
-	for (var i = 0; i < currentLength + 1; i++) {
-		for (var j = 0; j < expectedLength + 1; j++) {
-			if (i == 0 && j == 0) d[i][j] = 0;
-			else if (i == 0) {
+	for (let i = 0; i < currentLength + 1; i++) {
+		for (let j = 0; j < expectedLength + 1; j++) {
+			if (i === 0 && j === 0){
+                d[i][j] = 0;
+            }
+			else if (i === 0) {
 				d[i][j] = j;
 				moves[i][j] = "I";
 			}
-			else if (j == 0) {
+			else if (j === 0) {
 				d[i][j] = i;
 				moves[i][j] = "D";
 			}
 			else {
-				var c = diff(currentAnswer[i - 1], expectedAnswer[j - 1]);
-				var res1 = d[i][j - 1] + 1;
-				var res2 = d[i - 1][j] + 1;
-				var res3 = d[i - 1][j - 1] + c;
-				var result;
-				if (res1 <= res2 && res1 <= res3) {
-					result = res1;
+				const c = diff(currentAnswer[i - 1], expectedAnswer[j - 1]);
+				const insertCost = d[i][j - 1] + 1;
+				const deleteCost = d[i - 1][j] + 1;
+				const changeCost = d[i - 1][j - 1] + c;
+				if (insertCost <= deleteCost && insertCost <= changeCost) {
+					d[i][j] = insertCost;
 					moves[i][j] = "I";
+				} else if (deleteCost <= changeCost) {
+                    d[i][j] = deleteCost;
+                    moves[i][j] = "D";
 				} else {
-					if (res2 <= res3) {
-						result = res2;
-						moves[i][j] = "D";
-					} else {
-						result = res3;
-						moves[i][j] = (c == 0) ? "E" : "C";
-					}
+                    d[i][j] = changeCost;
+                    moves[i][j] = (c == 0) ? "E" : "C";
 				}
 
-				d[i][j] = result;
 			}
 		}
 	}
 
-
-	var i = currentLength;
-	var j = expectedLength;
-	editing = "";
+	let i = currentLength;
+	let j = expectedLength;
+	let editingString = "";
 
 	while (i > 0 || j > 0) {
-		editing = moves[i][j] + editing;
-		if (moves[i][j] == "I") {
+		editingString = moves[i][j] + editingString;
+		if (moves[i][j] === "I") {
 			j--;
-		} else if (moves[i][j] == "D") {
+		} else if (moves[i][j] === "D") {
 			i--;
 		} else {
 			i--;
@@ -136,7 +117,7 @@ function evaluateAnswer(currentAnswer, expectedAnswer, caseSensitive) {
 		}
 	}
 
-	return editing;
+	return editingString;
 }
 
 
